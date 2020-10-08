3d924e8a2e060ff877b4578f87d455da15567a3a
藤井沙綾 <say.f@fujiiisagoryounoMacBook-Pro.local>
2020-10-08 20:10:48 +0900

    [Finish]javascript5

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 $(function () {
-  $(".box1").css({
-    'background-color': '#0000FF',
-    'height': '100px'
+  $(".box1").slideDown(function () {
+    $(".box1")
+      .css({
+        "background-color": "#0000FF",
+        width: "200px",
+        height: "100px",
+      })
+      .slideUp();
   });
 });
